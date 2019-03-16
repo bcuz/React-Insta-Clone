c607cb0ca38e38ca47811f8d66119ed036ae7024
adam <bcuz@users.noreply.github.com>
2019-03-16 13:24:46 -0400

    bonus no posts found component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import data from './data';
 import PostContainer from './components/PostContainer/PostContainer';
+import NoPosts from './components/PostContainer/NoPosts';
 import SearchBar from './components/SearchBar/SearchBar';
 
 import './App.css';
@@ -40,10 +41,19 @@ class App extends Component {
   }
   
   render() {
+    let posts;
+    let results = this.state.data
+
+    if (results.length) {
+      posts = results.map(post => <PostContainer post={post} />)
+    } else {
+      posts = <NoPosts />
+    }
+
     return (
       <div>
           <SearchBar input={this.state.input} handleValChange={this.handleValChange} handleSubmit={this.handleSubmit} />
-          {this.state.data.map(post => <PostContainer post={post} />)}        
+          {posts}        
       </div>
     );
   }
